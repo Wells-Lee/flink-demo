11b92269a7483a15cd175299257e9369620f64ce
liq <liq@getui.com>
2020-05-07 23:43:48 +0800

    【Flink Demo】flink sink

---
+++ src/main/java/com/wells/flink/demo/sink/PrintSinkTest.java
@@ -0,0 +1,39 @@
+package com.wells.flink.demo.sink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
+import org.apache.flink.util.Collector;
+
+/**
+ * Description 
+ * Created by wells on 2020-05-07 23:31:31
+ */
+
+public class PrintSinkTest {
+    public static void main(String[] args) throws Exception {
+        String host = "127.0.0.1";
+        int port = 8090;
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> source = env.socketTextStream(host, port);
+        SingleOutputStreamOperator<Tuple2<String, Integer>> counts = source.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String line, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    collector.collect(new Tuple2<String, Integer>(word, 1));
+                }
+            }
+        }).keyBy(0).sum(1);
+
+//        counts.print().setParallelism(1);
+        // 这个效果与上面的 print 是一样的
+        counts.addSink(new PrintSinkFunction<>());
+
+        env.execute();
+    }
+}
